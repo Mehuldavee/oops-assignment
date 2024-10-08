d1547015a0075c4bd671bec49d8d42393254b2fc
Mehuldavee <mehuld185@gmail.com>
2024-10-08 15:53:53 +0530

    Add files via upload

---
+++ 1.concept oops/1.cpp
@@ -0,0 +1,12 @@
+//WAP TO PRINT "HELLO WORLD" USING C++.
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	cout<<"\nHELLO WORLD";
+	
+	return 0;
+}
+
---
+++ 1.concept oops/3.cpp
@@ -0,0 +1,44 @@
+//what is the difference oop & pop
+
+//oop example
+#include<iostream>
+using namespace std;
+
+class car
+{
+	
+	 public :
+	 	
+	 	void sound()
+	 	{
+	 		cout<<"\ncar sound";
+		 }
+};
+int main()
+{
+	car c1;
+	c1.sound();
+	return 0;
+}
+
+
+/*pop example
+
+#include<iostream>
+using namespace std;
+
+	void carsound()
+	{
+	
+	{
+		cout<<"\ncar sound";
+	}
+	
+	
+}
+int main()
+{
+
+    carsound();
+    return 0;
+}*/
